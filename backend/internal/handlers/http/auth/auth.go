658b07401d74f3a4a138c9de78b4677a2bfe235e
Газимов Диас <mirasgazimovv@icloud.com>
2026-07-07 23:15:18 +0500

    feat: added Second SignIn Handler

--- backend/internal/handlers/http/auth/auth.go
+++ backend/internal/handlers/http/auth/auth.go
@@ -23,6 +23,10 @@ type Creater interface {
 	CreateUser(ctx context.Context, user user.SignUpRequest) (uuid.UUID, error)
 }
 
+type Getter interface {
+	GetUser(ctx context.Context, u user.SignInRequest) (user.AuthUser, error)
+}
+
 const (
 	salt       = "dfhgsdfhgidu1224"
 	signingKey = "grkjk#4#%35FSFJlja#4353KSFjH"
@@ -115,16 +119,66 @@ func GenerateToken(ctx context.Context, id uuid.UUID) (string, error) {
 	return token.SignedString([]byte(signingKey))
 }
 
-// func NewSignIn(log *slog.Logger) http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		ctx := r.Context()
-// 		const op = "handlers/http/auth/NewSignIn"
-// 		log = log.With(
-// 			slog.String("op", op),
-// 			slog.String("request_id", middleware.GetReqID(r.Context())),
-// 		)
-// 	}
-// }
+func NewSignIn(log *slog.Logger, getter Getter) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		const op = "handlers/http/auth/NewSignIn"
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+		var signIn user.SignInRequest
+		err := render.DecodeJSON(r.Body, &signIn)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", signIn))
+
+		if err := validator.New().Struct(signIn); err != nil {
+			validateErr := err.(validator.ValidationErrors)
+			log.Error("invalid request", sl.Err(err))
+
+			render.Status(r, http.StatusBadRequest)
+			render.JSON(w, r, response.ValidationError(validateErr))
+
+			return
+		}
+
+		auth, err := getter.GetUser(ctx, signIn)
+		if err != nil {
+			if errors.Is(err, service.ErrInvalidCredentials) {
+				log.Info("User Authentication failed", slog.String("user", signIn.Username))
+
+				render.Status(r, http.StatusUnauthorized)
+				render.JSON(w, r, response.Error("Authentication failed"))
+
+				return
+			}
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, response.Error("Internal Server Error"))
+
+			return
+		}
+
+		jwttoken, err := GenerateToken(ctx, auth.ID)
+		if err != nil {
+			render.Status(r, http.StatusInternalServerError)
+			render.JSON(w, r, map[string]string{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		render.Status(r, http.StatusOK)
+		responseOK(w, r, jwttoken)
+	}
+}
 
 func responseOK(w http.ResponseWriter, r *http.Request, token string) {
 	render.JSON(w, r, TokenResponse{
