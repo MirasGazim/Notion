54b228204ecefcf9dc7e19764750825722053795
Газимов Диас <mirasgazimovv@icloud.com>
2026-07-09 16:45:39 +0500

    feat: delegate workspace repository call in workspaceService

--- backend/internal/service/workspace.go
+++ backend/internal/service/workspace.go
@@ -3,8 +3,14 @@ package service
 import (
 	"context"
 	"notion/internal/models/workspace"
+
+	"github.com/google/uuid"
 )
 
 func (s *workspaceService) Create(ctx context.Context, req workspace.CreateWorkspaceRequest) (*workspace.Workspace, error) {
 	return s.repo.Create(ctx, req)
+}
+
+func (s *workspaceService) GetWorkspaces(ctx context.Context, id uuid.UUID) ([]workspace.Workspace, error) {
+	return s.repo.GetWorkspaces(ctx, id)
 }
