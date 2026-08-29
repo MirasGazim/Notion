5f396e7c0745501e214f245ef07105fa857a9054
Газимов Диас <mirasgazimovv@icloud.com>
2026-08-29 19:20:28 +0500

    feat: Added some sentinel errors

--- backend/internal/repository/storage.go
+++ backend/internal/repository/storage.go
@@ -7,4 +7,7 @@ var (
 	ErrUserNotFound      = errors.New("user not found")
 	ErrUserNotDeleted    = errors.New("no rows deleted")
 	ErrWorkspaceNotFound = errors.New("workspace not found")
+	ErrBlockNotUpdated   = errors.New("no rows updated")
+	ErrBlockNotFound     = errors.New("block not found")
+	ErrNoFieldsToUpdate  = errors.New("no fields to update")
 )
